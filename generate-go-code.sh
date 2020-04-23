b5b128d8b3340593c0fe41d30067564534ca95eb
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-04-23 14:08:51 -0500

    Added basic protocol buffer

---
+++ generate-go-code.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Generating the necessary code from the .proto files..."
+echo "Updating GO111MODULE..."
+export GO111MODULE=on
+echo "PROTOC Version..."
+protoc --version
+echo "Generating proto codes..."
+
+#The argument of'plugins=grpc:.' in the --go_out tells the generator to create the necessary services
+protoc -I . --go_out=plugins=grpc:. ./basic-test/*.proto
+echo "Complete generating the code from the .proto files..."
